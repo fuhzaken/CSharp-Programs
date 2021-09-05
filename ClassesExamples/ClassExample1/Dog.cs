73c6211c493019984e739e98b054ff49d4b1a54b
fuhzaken <71949386+fuhzaken@users.noreply.github.com>
2021-09-05 10:55:52 -0400

    Added class examples.

---
+++ ClassesExamples/ClassExample1/Dog.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpUsingClass
+{
+    class Dog
+    {
+        // private  fields of the Dog class
+        private string dogOwner; // the name of the owner
+        private string dogName; // the name of the dog
+        private string dogBreed; // the breed of the dog
+
+        // constructor with no parameters
+        public Dog()
+        {
+            // nothing to set
+            // we will set the values in our main class/Program.cs
+        }
+
+        // constructor to set the dog's name and breed - 2 parameteres
+        public Dog(string dog, string breed)
+        {
+            dogName = dog;
+            dogBreed = breed;
+        }
+        // Reurns the dog's name
+        public string SetDogName
+        {
+            get
+            {
+                return dogName;
+            }
+            set
+            {
+                dogName = value;
+            }
+        }
+
+        // Returns the owner
+        public string SetOwner
+        {
+            get
+            {
+                return dogOwner;
+            }
+            set
+            {
+                dogOwner = value;
+            }
+        }
+        // Returns the dog breed
+        public string SetBreed
+        {
+            get
+            {
+                return dogBreed;
+            }
+            set
+            {
+                dogBreed = value;
+            }
+        }
+        // returns the string as a sentence
+        public override string ToString()
+        {
+            return $"My name is: {dogName} and I am a {dogBreed} type of dog, and {dogOwner} owns me.";
+        }
+        // returns the dog's bark
+        public static string DogBark()
+        {
+            return "Arf arf!";
+        }
+    }
+}
---
+++ ClassesExamples/ClassExample1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+    @author: xMinatwoah
+    @date: September 05, 2021
+    @description: Classes -> private fields, properties,
+    constructor, methods, ToString example.
+ */
+namespace CSharpUsingClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Object creation:
+                Dog name: Bella
+                Constructor used: Dog(dogName, dogBreed) - Constructor with two parameters/arguments"
+            */
+            Dog bella = new Dog("Bella", "Corgi");
+            bella.SetOwner = "xMinatwoah";
+            Console.WriteLine(bella);
+            Console.WriteLine(Dog.DogBark());
+            
+            /*  Object creation:
+                Dog name: Browny
+                Constructor used: Dog() - Constructor with no parameters/arguments
+            */
+            Dog dog = new Dog();
+            dog.SetDogName = "Browny";
+            dog.SetBreed = "Husky";
+            dog.SetOwner = "Nelson";
+            Console.WriteLine(dog);
+            Console.WriteLine(Dog.DogBark());
+            Console.ReadKey();
+        }
+    }
+}
