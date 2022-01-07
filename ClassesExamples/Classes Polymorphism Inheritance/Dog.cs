4306d170789f52d121cd65b9df8519e8aacd51f7
fuhzaken <71949386+fuhzaken@users.noreply.github.com>
2022-01-06 23:17:38 -0500

    Update Dog.cs

--- ClassesExamples/Classes Polymorphism Inheritance/Dog.cs
+++ ClassesExamples/Classes Polymorphism Inheritance/Dog.cs
@@ -3,7 +3,7 @@ namespace Classes1
 {
     public class Dog : Animal
     {
-        // Practice: Classes, Polymorphism, and Inheritance
+        // Practice: Classes, Encapsulation, Polymorphism, and Inheritance
         // Author: @kentzyzxc/fuhzaken
 
         public Dog() : base()
