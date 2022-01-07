d38e41e2f26b6c55c9b4fb81518ddff42f1578fc
fuhzaken <71949386+fuhzaken@users.noreply.github.com>
2022-01-06 23:17:24 -0500

    Update Cat.cs

--- ClassesExamples/Classes Polymorphism Inheritance/Cat.cs
+++ ClassesExamples/Classes Polymorphism Inheritance/Cat.cs
@@ -3,7 +3,7 @@ namespace Classes1
 {
     public class Cat : Animal
     {
-        // Practice: Classes, Polymorphism, and Inheritance
+        // Practice: Classes, Encapsulation, Polymorphism, and Inheritance
         // Author: @kentzyzxc/fuhzaken
 
         public Cat() : base()
